e611298377f68ac9a9fdea17ce9f0ac097264340
fatih-git <72505641+fatih-git@users.noreply.github.com>
2022-10-28 00:49:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace If
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            bool condition = true;
+
+            if (condition)
+            {
+                Console.WriteLine("Değişken: true");
+            }
+            else
+            {
+                Console.WriteLine("Değişken: false");
+            }
+
+        }
+
+    }
+
+}
